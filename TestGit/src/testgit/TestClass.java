6fe8714516b3ada2afb4bb46c455e0c590ae79d0
Miroslav Liubomirov Metodiev <miroslav.l.metodiev>
2013-05-29 09:45:14 +0300

    Test commit

---
+++ TestGit/src/testgit/TestClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgit;
+/**
+ *
+ * @author miroslav.metodiev
+ */
+public class TestClass {
+    
+    public void PrintMethod(){
+        System.out.println("Test print method");
+    }
+    
+}
